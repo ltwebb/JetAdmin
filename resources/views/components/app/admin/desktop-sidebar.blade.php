c2f54785a8837dbe7015ba141946b5714db0e700
Lt Webb <ltw839@gmail.com>
2024-07-26 11:14:41 -0400

    Add User to Navigation

--- resources/views/components/app/admin/desktop-sidebar.blade.php
+++ resources/views/components/app/admin/desktop-sidebar.blade.php
@@ -18,9 +18,10 @@
                 </x-app.admin.side-link>
             </li>
             <li class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
-                <button @click="open = ! open"
+
+                    <button @click="open = ! open"
                     class="inline-flex items-center w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start font-semibold text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out"
-                    aria-haspopup="true">
+                     aria-haspopup="true">
                     <span class="inline-flex items-center">
                         <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
@@ -28,7 +29,7 @@
                                 d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z">
                             </path>
                         </svg>
-                        <span class="ml-4">Pages</span>
+                        <span class="ml-4">User Management</span>
                     </span>
                     <svg :class="{ 'rotate-180': open }" class="ml-2 w-4 h-4 transition-transform duration-200"
                         aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -37,6 +38,7 @@
                             clip-rule="evenodd"></path>
                     </svg>
                 </button>
+
                 <div x-show="open" x-transition:enter="transition ease-out duration-200"
                     x-transition:enter-start="transform opacity-0 scale-95"
                     x-transition:enter-end="transform opacity-100 scale-100"
@@ -55,7 +57,7 @@
                             aria-label="submenu">
                             <li
                                 class="px-2 py-1 transition-colors duration-150 hover:text-gray-900 dark:hover:text-gray-200">
-                                <x-app.admin.side-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                                <x-app.admin.side-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
                                     <svg class="mr-2 w-5 h-5" aria-hidden="true" fill="none"
                                         stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         viewBox="0 0 24 24" stroke="currentColor">
@@ -64,12 +66,12 @@
                                         </path>
                                     </svg>
 
-                                    {{ __('Dashboard') }}
+                                    {{ __('Users') }}
                                 </x-app.admin.side-link>
                             </li>
                             <li
                                 class="px-2 py-1 transition-colors duration-150 hover:text-gray-900 dark:hover:text-gray-200">
-                                <x-app.admin.side-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                                <x-app.admin.side-link href="{{ route('users.create') }}" :active="request()->routeIs('users.create')">
                                     <svg class="mr-2 w-5 h-5" aria-hidden="true" fill="none"
                                         stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         viewBox="0 0 24 24" stroke="currentColor">
@@ -77,7 +79,7 @@
                                             d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z">
                                         </path>
                                     </svg>
-                                    {{ __('Dashboard') }}
+                                    {{ __('Create User') }}
                                 </x-app.admin.side-link>
                             </li>
 
@@ -85,16 +87,7 @@
                     </div>
                 </div>
             </li>
-            <li class="relative">
-                <x-app.admin.side-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" class="mr-2 w-5 h-5">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
-                    </svg>
-                    {{ __('Dashboard') }}
-                </x-app.admin.side-link>
-            </li>
+
         </ul>
 
     </div>
