bcb843297ed85aac3a703e16235c6a2eb860a24c
Lt Webb <ltw839@gmail.com>
2024-07-27 15:17:04 -0400

    Add multiple to select input

--- resources/views/app/admin/users/edit.blade.php
+++ resources/views/app/admin/users/edit.blade.php
@@ -45,8 +45,8 @@
 
                         <div class="px-4 py-5 w-1/2 sm:p-6">
                             <x-label for="roles" class="block font-medium text-sm text-gray-700">Roles</x-label>
-                            <select name="roles[]" id="roles" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option selected>Select user role</option>
+                            <select multiple name="roles[]" id="roles" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
                                 @foreach($roles as $id => $role)
                                     <option value="{{ $id }}"{{ in_array($id, old('roles', $user->roles->pluck('id')->toArray())) ? ' selected' : '' }}>
                                         {{ $role }}
