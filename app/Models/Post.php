718d43da79557a7e01ea9723e1f1f61efacd2ac8
Lori Webb <ltw839@gmail.com>
2024-08-05 15:31:40 -0400

    Add thumbnail manipulatin to model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\Conversions\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
 
 class Post extends Model implements HasMedia
 {
@@ -13,5 +17,14 @@ class Post extends Model implements HasMedia
     use InteractsWithMedia;
 
     protected $fillable = ['title'];
+
+    public function registerMediaConversions(?Media $media = null): void
+{
+    $this
+        ->addMediaConversion('thumbnail')
+        //->fit(Fit::Contain, 300, 300)
+        ->fit(Manipulations::FIT_FILL, 200, 200)
+        ->nonQueued();
+}
 
 }
